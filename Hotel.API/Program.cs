d2792c14d1b3fabbf1588f66c1b602fec60f1e84
Gerg0 <tothgergo602@gmail.com>
2024-03-31 15:33:48 +0200

    feat added jwt authentication to swagger

--- Hotel.API/Program.cs
+++ Hotel.API/Program.cs
@@ -15,6 +15,8 @@ using Asp.Versioning.ApiExplorer;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 using Microsoft.AspNetCore.OData;
+using Microsoft.OpenApi.Models;
+using System.ComponentModel;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,7 +35,34 @@ builder.Services.AddIdentityCore<ApiUser>()
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options=>{
+    options.SwaggerDoc("v1", new OpenApiInfo{
+        Title="Hotel Listing API", Version = "v1"
+    });
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme{
+        Description=@"JWT Authorization header using the Bearer scheme.
+        Enter 'Bearer' [space] and then your token in the text input below.
+        Example: 'Bearer 123456abcdef",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement{
+        {
+            new OpenApiSecurityScheme{
+                Reference =new OpenApiReference{
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme,
+                },
+                Scheme = "Oauth2",
+                Name = JwtBearerDefaults.AuthenticationScheme,
+                In = ParameterLocation.Header
+            },
+            new List<string>()  
+        }
+    });
+});
 
 
 
